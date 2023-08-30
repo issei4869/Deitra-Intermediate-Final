b95b70c0af9a1b4ef0f23de40ad7ed490696a3f6
長谷川一輝 <e3nr3hwtl.87269.yu@gmail.com>
2023-08-31 00:42:01 +0900

    header完了

---
+++ js/script.js
@@ -0,0 +1,69 @@
+
+jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+
+  var topBtn = $('.pagetop');
+  topBtn.hide();
+
+  // ボタンの表示設定
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 70) {
+      // 指定px以上のスクロールでボタンを表示
+      topBtn.fadeIn();
+    } else {
+      // 画面が指定pxより上ならボタンを非表示
+      topBtn.fadeOut();
+    }
+  });
+
+  // ボタンをクリックしたらスクロールして上に戻る
+  topBtn.click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 300, 'swing');
+    return false;
+  });
+
+  //ドロワーメニュー
+  $(".js-hamburger").click(function () {
+    //js-hamburgerをクリックしたとき、is-activeクラスがある場合
+    if ($('.js-hamburger').hasClass('is-active')) {
+      //is-activeクラスを外す
+      $('.js-hamburger').removeClass("is-active");
+      $('.js-background').removeClass("is-active");
+      //$("html").toggleClass("is-fixed");
+      $(".js-sp-nav").removeClass("is-active");
+    //is-activeクラスがない場合
+    } else {
+      ////is-activeクラスを追加する
+      $('.js-hamburger').addClass("is-active");
+      $('.js-background').addClass("is-active");
+      $(".js-sp-nav").addClass("is-active");
+    }
+  });
+
+
+
+  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
+
+  $(document).on('click', 'a[href*="#"]', function () {
+    let time = 400;
+    let header = $('header').innerHeight();
+    let target = $(this.hash);
+    if (!target.length) return;
+    let targetY = target.offset().top - header;
+    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+    return false;
+  });
+
+  var swiper = new Swiper(".js-works-swiper", {
+    pagination: {
+      el: ".js-works-pagination",
+    },
+    clickable: true,
+    loop: true,
+    // autoplay: {
+    //   delay: 3000, //数値/1000秒
+    // },
+  });
+
+});
